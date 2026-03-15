94b838ee27da6122894942b3c6eb6cc169a92027
hcsdrose <crose@abre.io>
2026-03-15 10:38:42 -0400

    commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <HomePage />
+</template>
+
+<script>
+import HomePage from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+  background: #fff;
+  min-height: 100vh;
+  margin-top: 0;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div class="homepage-plain">
+    <h1>Christopher Rose</h1>
+    <h2>Educator & Executive</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style scoped>
+.homepage-plain {
+  background: #fff;
+  color: #000;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Rubik', Arial, Helvetica, sans-serif;
+}
+h1, h2 {
+  color: #000;
+  margin: 0;
+  font-family: 'Rubik', Arial, Helvetica, sans-serif;
+}
+h1 {
+  font-size: 2.5rem;
+  font-weight: 500;
+  margin-bottom: 0.5rem;
+}
+h2 {
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: #666;
+  letter-spacing: -1px;
+}
+</style>
