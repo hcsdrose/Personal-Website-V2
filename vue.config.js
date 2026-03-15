ff1f7f228fd2ee4e8c169ebc7ad5d3dbd8e4fbba
hcsdrose <crose@abre.io>
2026-03-15 10:44:39 -0400

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/personal-website-v2/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/Personal-Website-V2/' : '/'
 })
